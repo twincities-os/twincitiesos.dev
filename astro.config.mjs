ea6a987ef12ec7f5164580ba443f470a844d6818
Brandon Ly <brandonly@lostcoding.com>
2025-02-25 08:20:51 -0600

    feat: run formatter and linter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-  integrations: [icon()],
+	adapter: cloudflare(),
+	integrations: [icon()],
 });
