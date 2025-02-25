cf84dab7d09213ac6886b8b6250c78d7ca4b2f83
Brandon Ly <brandonly@lostcoding.com>
2025-02-25 08:58:07 -0600

    feat: create github action workflow (#27)

--- constants.ts
+++ constants.ts
@@ -1,2 +1,3 @@
-export const DISCORD_LINK = 'https://discord.gg/awR9AWc6xx'
-export const MEETUP_LINK = 'https://www.meetup.com/twin-cities-open-source-developers'
+export const DISCORD_LINK = "https://discord.gg/awR9AWc6xx";
+export const MEETUP_LINK =
+	"https://www.meetup.com/twin-cities-open-source-developers";
