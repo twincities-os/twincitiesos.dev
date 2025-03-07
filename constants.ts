20dd0efeda47de087d302d68ea55f7cda9f6d06a
Chase Johnson <cjohnson19@pm.me>
2025-03-06 19:16:28 -0600

    Add link to Github organization on welcome page (#32)

--- constants.ts
+++ constants.ts
@@ -1,3 +1,4 @@
 export const DISCORD_LINK = "https://discord.gg/awR9AWc6xx";
+export const GITHUB_LINK = "https://github.com/twincities-os";
 export const MEETUP_LINK =
 	"https://www.meetup.com/twin-cities-open-source-developers";
