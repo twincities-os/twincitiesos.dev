70dfc49870b143c54eb521dd7238073478fd1375
Nick Latcham <nick.latcham@proton.me>
2025-03-17 14:09:22 -0500

    feat: add mdx plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-	adapter: cloudflare(),
-	integrations: [icon()],
+    adapter: cloudflare(),
+    integrations: [icon(), mdx()],
 });
