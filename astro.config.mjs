46e5f07293c1832f3240ae44d024e1b2053a6bff
Brandon Ly <brandonly@lostcoding.com>
2025-03-25 08:07:41 -0500

    feat: run formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: cloudflare(),
-    integrations: [icon(), mdx()],
+	adapter: cloudflare(),
+	integrations: [icon(), mdx()],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,15 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    author: z.string(),
-    slug: z.string(),
-    pubDate: z.coerce.date(),
-  })
+	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		author: z.string(),
+		slug: z.string(),
+		pubDate: z.coerce.date(),
+	}),
 });
 
 export const collections = { blog };
